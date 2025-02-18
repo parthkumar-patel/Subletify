5bc473fbcfd968c9e98f0d948796c997b8555129
Parth <parthp15@yahoo.com>
2025-02-17 17:53:15 -0800

    alignment fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import WaitlistPage from "./waitlist-page";
 
 export default function Home() {
   return (
-    <div className="items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
+    <div className="min-h-screen font-[family-name:var(--font-geist-sans)]">
       <WaitlistPage />
     </div>
   );
